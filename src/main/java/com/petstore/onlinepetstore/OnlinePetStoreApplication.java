53dc47f95a044a99401b82109625ac8aaa311b1a
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-09-30 14:11:06 +0300

    initial commit

---
+++ src/main/java/com/petstore/onlinepetstore/OnlinePetStoreApplication.java
@@ -0,0 +1,13 @@
+package com.petstore.onlinepetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePetStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinePetStoreApplication.class, args);
+    }
+
+}
